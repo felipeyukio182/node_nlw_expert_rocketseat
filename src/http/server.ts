314f139926fa2d8edf2d131c475ed3a0b22b3ca3
felipeyukio182 <felipeyukio182@gmail.com>
2024-02-08 08:28:42 -0300

    feat: criado rotas para votacao

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,20 @@
-import fastify from 'fastify';
-import {PrismaClient} from '@prisma/client';
-import {z} from 'zod';
+import fastify from "fastify";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import cookie from '@fastify/cookie';
 
 const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(cookie, {
+	secret: 'polls-app-nlw',
+	hook: 'onRequest',
+});
 
-app.post('/polls', async (req, reply) => {
-    const createPollBody = z.object({
-        title: z.string(),
-    });
-    const {title} = createPollBody.parse(req.body);
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    });
-
-    return reply.status(201).send({
-        pollId: poll.id,
-    });
-})
-
-app.listen({port: 3333}).then(() => {
-    console.log('HTTP server running!');
-})
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!");
+});
